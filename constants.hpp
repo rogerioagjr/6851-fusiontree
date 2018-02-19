749a0c02303180b98cc03f85c0a73a75b2b7b135
Rogerio A Guimaraes Jr <rjunior@mit.edu>
2018-02-18 22:40:56 -0500

    formatting

--- constants.hpp
+++ constants.hpp
@@ -10,9 +10,9 @@
 
 #include <stdio.h>
 
-#define WSIZE 300 // Actual size the big_int must have
-#define WVAR 243 // Value of w, the maximum number of bits a big_int may have
-#define PSIZE 300 // Number of bits printed in cout << big_int
-#define K 3 // O(WVAR^(1/5))
+#define WSIZE 300  // Actual size the big_int must have
+#define WVAR 243   // Value of w, the maximum number of bits a big_int may have
+#define PSIZE 300  // Number of bits printed in cout << big_int
+#define K 3        // O(WVAR^(1/5))
 
 #endif /* constants_hpp */
