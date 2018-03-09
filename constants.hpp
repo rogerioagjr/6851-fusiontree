9e5517115da81b7e63ed0768f07d72fcf2267d5f
Rogerio A Guimaraes Jr <rjunior@mit.edu>
2018-03-09 00:47:36 -0500

    version without online operator <<

--- constants.hpp
+++ constants.hpp
@@ -10,10 +10,15 @@
 
 #include <stdio.h>
 
-#define WSIZE 330  // Size the big_int must have - O(K^5+K^4)
+/*#define WSIZE 330  // Size the big_int must have - O(K^5+K^4)
+#define WVAR 243  // Value of w, the maximum number of bits of an element of a
+fusion tree #define PSIZE 20  // Number of bits printed in cout << big_int
+#define K 3       // Maximum size of a fusion tree - O(WVAR^(1/5))*/
+
+#define WSIZE 3750  // Size the big_int must have - O(K^5+K^4)
 #define WVAR \
-  243  // Value of w, the maximum number of bits of an element of a fusion tree
+  3125  // Value of w, the maximum number of bits of an element of a fusion tree
 #define PSIZE 20  // Number of bits printed in cout << big_int
-#define K 3       // Maximum size of a fusion tree - O(WVAR^(1/5))
+#define K 5       // Maximum size of a fusion tree - O(WVAR^(1/5))
 
 #endif /* constants_hpp */
